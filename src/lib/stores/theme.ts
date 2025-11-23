40d2bebf1d9841f2fe2fe2cb237ffe18066b94d3
Pavels <dnicer3@gmail.com>
2025-11-23 15:41:24 +0200

    Making theme easier

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,4 +1,4 @@
-import storage from '$lib/helpers/storage';
+// import storage from '$lib/helpers/storage';
 import { Moon, Sun } from '@lucide/svelte';
 import { writable } from 'svelte/store';
 
@@ -16,28 +16,33 @@ export const themesOptions = Object.values(Theme).map((theme) => ({
 const isDarkThemePreffered = () =>
 	window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-async function initTheme(): Promise<Theme> {
-	let theme = await storage.get<Theme | null>('theme');
+export const currentTheme = writable(isDarkThemePreffered() ? Theme.Dark : Theme.Light);
 
-	if (!theme) {
-		theme = isDarkThemePreffered() ? Theme.Dark : Theme.Light;
-	}
-
-	await applyTheme(theme);
-	return theme;
-}
-
-export const currentTheme = writable(await initTheme());
-
-async function applyTheme(theme: Theme) {
-	const storedTheme = await storage.get<Theme | null>('theme');
-	if (storedTheme !== theme) {
-		await storage.set('theme', theme);
-	}
-	document.documentElement.classList.toggle('dark', theme === Theme.Dark);
-}
-
-export async function updateTheme(theme: Theme) {
+export function updateTheme(theme: Theme) {
 	currentTheme.set(theme);
-	await applyTheme(theme);
 }
+// async function initTheme(): Promise<Theme> {
+// 	let theme = await storage.get<Theme | null>('theme');
+
+// 	if (!theme) {
+// 		theme = isDarkThemePreffered() ? Theme.Dark : Theme.Light;
+// 	}
+
+// 	await applyTheme(theme);
+// 	return theme;
+// }
+
+// export const currentTheme = writable(await initTheme());
+
+// async function applyTheme(theme: Theme) {
+// 	const storedTheme = await storage.get<Theme | null>('theme');
+// 	if (storedTheme !== theme) {
+// 		await storage.set('theme', theme);
+// 	}
+// 	document.documentElement.classList.toggle('dark', theme === Theme.Dark);
+// }
+
+// export async function updateTheme(theme: Theme) {
+// 	currentTheme.set(theme);
+// 	await applyTheme(theme);
+// }
